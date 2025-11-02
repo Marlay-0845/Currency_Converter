b7c26c56e17abc768a71cd42d8ba92a440ad96ce
Marlay-0845 <denial.xpamsov@gmail.com>
2025-11-02 15:38:04 +0100

    main.py

--- Conventer/main.py
+++ Conventer/main.py
@@ -2,15 +2,27 @@ from Conventer.api_handler_conventer import handler
 from Conventer.storage import storage
 
 
-from_currency_choise = input("Enter a currency(from): ")
-to_currency_choise = input("Enter a currency(to): ")
-amount_choise = input("Enter amount: ")
+while True:
+    from_currency_choise = input("Enter a currency(from): ")
+    to_currency_choise = input("Enter a currency(to): ")
+    
+    try:
+        amount_choise = float(input("Enter amount: "))
+        if amount_choise <= 0:
+            print("The amount must be greater than zero!")
+            continue
+        break
 
+    except ValueError:
+        print("Enter a number!")
 
 date = handler(fromcurrency=from_currency_choise, tocurrency=to_currency_choise, amount=amount_choise)
 
-storage(date=date, to_currency=to_currency_choise)
 
+if date:
+    storage(date=date, to_currency=to_currency_choise)
+else:
+    print("Data not received!")
 
 
 
