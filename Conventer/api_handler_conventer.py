eb8306d71905c500705b7758f0c1324d4e88c001
Marlay-0845 <denial.xpamsov@gmail.com>
2025-11-02 15:38:27 +0100

    api_handler_conventer.py

--- Conventer/api_handler_conventer.py
+++ Conventer/api_handler_conventer.py
@@ -1,22 +1,27 @@
 import requests
 
 
+
 def handler(fromcurrency, tocurrency, amount):
     try:
         response = requests.get(f"https://api.frankfurter.dev/v1/latest?from={fromcurrency}&to={tocurrency}&amount={amount}")
         response.raise_for_status()
-        print("Данные получены успешно!")
+        print("Data received successfully!")
+
+        return response.json()
 
     except requests.exceptions.HTTPError as e:
-        print(f"❌ Ошибка HTTP: {e.response.status_code} — {e.response.reason}")
+        print(f"HTTP error: {e.response.status_code} — {e.response.reason}")
+        return None
 
     except requests.exceptions.ConnectionError:
-        print("❌ Ошибка соединения. Проверьте интернет или URL.")
+        print("Connection error. Check your internet connection or URL.")
+        return None
 
     except requests.exceptions.Timeout:
-        print("❌ Истекло время ожидания ответа от сервера.")
+        print("The timeout period for waiting for a response from the server has expired.")
+        return None
 
     except Exception as e:
-        print(f"⚠️ Непредвиденная ошибка: {type(e).__name__} — {e}")
-
-    return response.json()
+        print(f"Unexpected error: {type(e).__name__} — {e}")
+        return None
