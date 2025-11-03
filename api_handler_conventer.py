ad4e1edd419e19e45ff6f2850386198708e75de5
Marlay-0845 <denial.xpamsov@gmail.com>
2025-11-03 19:49:44 +0100

    Integrate logging into currency handler

--- api_handler_conventer.py
+++ api_handler_conventer.py
@@ -1,27 +1,30 @@
 import requests
 
 
+from Conventer.logger_config import setup_logger
+
+
 
 def handler(fromcurrency, tocurrency, amount):
     try:
         response = requests.get(f"https://api.frankfurter.dev/v1/latest?from={fromcurrency}&to={tocurrency}&amount={amount}")
         response.raise_for_status()
-        print("Data received successfully!")
+        setup_logger().info("Data received successfully!")
 
         return response.json()
 
     except requests.exceptions.HTTPError as e:
-        print(f"HTTP error: {e.response.status_code} — {e.response.reason}")
+        setup_logger().error(f"HTTP error: {e.response.status_code} — {e.response.reason}")
         return None
 
     except requests.exceptions.ConnectionError:
-        print("Connection error. Check your internet connection or URL.")
+        setup_logger().error("Connection error. Check your internet connection or URL.")
         return None
 
     except requests.exceptions.Timeout:
-        print("The timeout period for waiting for a response from the server has expired.")
+        setup_logger().error("The timeout period for waiting for a response from the server has expired.")
         return None
 
     except Exception as e:
-        print(f"Unexpected error: {type(e).__name__} — {e}")
+        setup_logger().error(f"Unexpected error: {type(e).__name__} — {e}")
         return None
